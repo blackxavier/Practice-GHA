376e41878f0b15d6cae9262274222c13ec9c5308
blackxavier <ogbuchiarinze01@gmail.com>
2024-07-02 22:59:47 +0100

    nc

---
+++ upload/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from .models import Post
+
+
+class PostTests(TestCase):
+
+    def setUp(self):
+        self.blog = Post.objects.create(
+            title="just a test", author="Arinze", slug="arinze-test"
+        )
+        self.blog.save()
+
+    def test_post_content(self):
+        self.assertEqual(self.blog.title, "just a test")
+        self.assertEqual(self.blog.author, "Arinze")
+        self.assertEqual(self.blog.slug, "arinze-test")
+        self.assertEqual(str(self.blog), "just a test")
